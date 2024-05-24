ba5dae855d2906de921146894ecb285af2652321
JerkJerky <jerkjerkyar@outlook.com>
2024-05-24 20:11:56 +0200

    Census API RateLimit.

--- static-api/src/main/java/com/github/jerkjerky/census/collections/client/StaticContentClient.java
+++ static-api/src/main/java/com/github/jerkjerky/census/collections/client/StaticContentClient.java
@@ -5,12 +5,17 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.github.jerkjerky.census.collections.common.CachingRedirect;
+import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.github.resilience4j.ratelimiter.RateLimiterConfig;
 import lombok.Getter;
 import lombok.SneakyThrows;
-import okhttp3.*;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
@@ -22,12 +27,18 @@ public class StaticContentClient {
 
     public static final Duration CACHE_INVALIDATION_TIME = Duration.of(2, ChronoUnit.HOURS);
 
+    private final RateLimiter rateLimiter = RateLimiter.of("census-static-api-requests", RateLimiterConfig.custom()
+            .limitForPeriod(1)
+            .timeoutDuration(Duration.of(10, ChronoUnit.SECONDS))
+            .limitRefreshPeriod(Duration.of(250, ChronoUnit.MILLIS))
+            .build());
     private final ObjectMapper objectMapper = new ObjectMapper()
             .registerModule(new JavaTimeModule())
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     private final String serviceId;
     private final OkHttpClient httpClient;
     private final Map<Class<?>, CachingRedirect> cachingRedirectMap;
+
 
     @Getter
     private final OutfitClient outfitClient;
@@ -57,8 +68,13 @@ public class StaticContentClient {
 
     @SneakyThrows
     <T> T makeRequest(Request request, TypeReference<T> typeReference) {
+        byte[] bytes = rateLimiter.executeCheckedSupplier(() -> makeRequestInner(request));
+        return objectMapper.readValue(bytes, typeReference);
+    }
+
+    protected byte[] makeRequestInner(Request request) throws IOException {
         try(Response response = this.httpClient.newCall(request).execute()){
-            return objectMapper.readValue(response.body().bytes(), typeReference);
+            return response.body().bytes();
         } catch (Exception e){
             logger.error("Failure during access to census via HTTP", e);
             throw e;
